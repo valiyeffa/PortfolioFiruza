cd842dd1469e76a3cb8528b95daa38c6fa432361
valiyeffa <firuzeveliyeva2@gmail.com>
2026-05-25 13:07:46 +0400

    mini done

--- src/app/features/home/main/main.ts
+++ src/app/features/home/main/main.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: `./main.scss`,
 })
 export class Main {
+  @ViewChild('sect', { static: true }) sect!: ElementRef<HTMLDivElement>;
 
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const rect = this.sect.nativeElement.getBoundingClientRect();
+
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    this.sect.nativeElement.style.setProperty('--x', `${x}px`);
+    this.sect.nativeElement.style.setProperty('--y', `${y}px`);
+  }
 }
