d225675a594021992e78a873e67f323440df1d2b
valiyeffa <firuzeveliyeva2@gmail.com>
2026-05-26 13:19:17 +0400

    contact section added

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,10 +3,11 @@ import { Main } from "./main/main";
 import { About } from "./about/about";
 import { Projects } from "./projects/projects";
 import { Journey } from "./journey/journey";
+import { Contact } from "./contact/contact";
 
 @Component({
   selector: 'app-home',
-  imports: [Main, About, Projects, Journey],
+  imports: [Main, About, Projects, Journey, Contact],
   templateUrl: './home.html',
   styles: ``,
 })
